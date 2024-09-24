33a2528bd168fbc8ef7622a24be0dcf4f27b07d4
Siddharth <siddharth12dhiman@gmail.com>
2024-09-24 18:34:47 +0530

    added minmax algo to computer player for human vs ai mode

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -7,22 +7,6 @@ using namespace std;
 
 const int N = 1e9 + 7;
 
-class AiMove
-{
-public:
-    int row, col, score;
-    AiMove(int score)
-    {
-        this->score = score;
-    }
-    AiMove(int x, int y, int score)
-    {
-        this->row = x;
-        this->col = y;
-        this->score = score;
-    }
-};
-
 bool isSlotValid(vector<vector<char>> &board, int row, int col)
 {
     return board[row][col] >= '1' && board[row][col] <= '9';
@@ -166,133 +150,115 @@ void twoPlayer(vector<vector<char>> &board)
     cout << "It's a Tie !!!!" << endl;
 }
 
-AiMove aiMove(vector<vector<char>> &board, char &aiMarker, char &playerMarker, bool isAi)
+bool isBoardFull(vector<vector<char>> &board)
 {
-    // Base case : Check for win
-    if (checkWinner(board))
-    {
-        return isAi ? AiMove(10) : AiMove(-10);
-    }
-
-    bool movesLeft = false;
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
-            if (isSlotValid(board, i, j))
-            {
-                movesLeft = true;
-                break;
-            }
-    if (!movesLeft)
-        return AiMove(0);
-
-    vector<AiMove> moves;
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            if (isSlotValid(board, i, j))
-            {
-                char temp = board[i][j];
-                AiMove move(i, j, 0);
-                board[i][j] = isAi ? aiMarker : playerMarker;
-                if (isAi == true)
-                    move.score += aiMove(board, aiMarker, playerMarker, !isAi).score;
-                else
-                    move.score += aiMove(board, aiMarker, playerMarker, isAi).score;
-
-                // backtracking
-                board[i][j] = temp;
-                moves.push_back(move);
-            }
-
-    int bestMove;
-    if (isAi)
-    {
-        int bestScore = INT_MIN;
-        for (int i = 0; i < moves.size(); i++)
-            if (moves[i].score > bestScore)
-            {
-                bestMove = i;
-                bestScore = moves[i].score;
-            }
-        cout << "AI best score " << moves[bestMove].score << endl;
-    }
-    else
-    {
-        int bestScore = INT_MAX;
-        for (int i = 0; i < moves.size(); i++)
-            if (moves[i].score < bestScore)
-            {
-                bestMove = i;
-                bestScore = moves[i].score;
-            }
-        cout << "Player best move " << moves[bestMove].row << " " << moves[bestMove].col << endl;
-        cout << "Player best score " << moves[bestMove].score << endl;
-    }
-    return moves[bestMove];
+            if (board[i][j] >= '1' && board[i][j] <= '9')
+                return false;
+    return true;
 }
 
-void playerMove(vector<vector<char>> &board, char playerMarker)
+int minimax(vector<vector<char>> &board, bool isMaximizing, char &ai_marker, char &player_marker, int depth)
 {
-    int slot = getSlot(board);
-    placeMarker(slot, playerMarker, board);
-    drawBoard(board);
+    if (checkWinner(board))
+        return isMaximizing ? -10 : 10;
+    if (isBoardFull(board))
+        return 0;
+
+    int bestScore = isMaximizing ? -1e9 : 1e9;
+    char currentMarker = isMaximizing ? ai_marker : player_marker;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] >= '1' && board[i][j] <= '9')
+            {
+                char backup = board[i][j];
+                board[i][j] = currentMarker;
+
+                int score = minimax(board, !isMaximizing, ai_marker, player_marker, depth + 1);
+
+                bestScore = isMaximizing ? max(score, bestScore) : min(score, bestScore);
+
+                board[i][j] = backup; // undo the move
+            }
+    return bestScore;
+}
+
+int findBestMove(vector<vector<char>> &board, char &ai_marker, char &player_marker)
+{
+    int bestScore = -1e9;
+    int bestMove = -1;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] >= '1' && board[i][j] <= '9') // if spot available
+            {
+                char temp = board[i][j];
+                board[i][j] = ai_marker;
+
+                int score = minimax(board, false, ai_marker, player_marker, 0);
+
+                board[i][j] = temp; // backtrack
+
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestMove = (i * 3) + j + 1;
+                }
+            }
+    return bestMove;
 }
 
 void singlePlayer(vector<vector<char>> &board)
 {
-    cout << "Single Player Mode" << endl;
-    char player_marker, ai_marker = 'O';
+    cout << "Single Player Mode (You vs A.I)" << endl
+         << endl;
+
+    int player = 1;
+
+    char marker, ai_marker = 'O';
 
     cout << "Choose O or X : ";
-    cin >> player_marker;
+    cin >> marker;
 
-    if (player_marker == 'o' || player_marker == 'O' || player_marker == '0')
+    if (marker == 'o' || marker == 'O' || marker == '0')
     {
-        player_marker = 'O';
+        marker = 'O';
         ai_marker = 'X';
     }
     else
-        player_marker = 'X';
+        marker = 'X';
 
-    cout << "Player : " << player_marker << endl;
-    cout << "Computer: " << ai_marker << endl;
-    cout << "Enter the key corresponding to the number in the board to enter your move" << endl
-         << endl;
-    cout << "Game Started" << endl;
-
-    int player = 1, ai_player = 2;
     for (int i = 0; i < 9; i++)
     {
-        bool isAi = true;
-        if (i & 1)
-        {
-            AiMove bestMove = aiMove(board, ai_marker, player_marker, isAi);
-            board[bestMove.row][bestMove.col] = ai_marker;
-        }
-        else
+        if (!(i & 1))
         {
             cout << "Your turn" << endl;
             cout << "Enter the slot you want to place your marker: ";
-            playerMove(board, player_marker);
+            int slot = getSlot(board);
+            placeMarker(slot, marker, board);
+        }
+        else
+        {
+            cout << "Computer's turn (Player " << player << ")" << endl;
+            int bestMove = findBestMove(board, ai_marker, marker);
+            placeMarker(bestMove, ai_marker, board);
+            cout << "Computer placed marker at slot " << bestMove << endl;
         }
 
+        drawBoard(board);
         if (checkWinner(board))
         {
             if (i & 1)
-            {
-                cout << "Computer wins!!!!" << endl;
-                cout << "Better Luck Next Time!!!" << endl;
-                return;
-            }
+                cout << "Computer wins!!" << endl
+                     << "Better Luck Next Time!!";
             else
-            {
-                cout << "Player wins!!!!" << endl;
-                cout << "Congratulations!!!" << endl;
-                return;
-            }
+                cout << "You win!!!!" << endl;
+            return;
         }
     }
     cout << "It's a Tie !!!!" << endl;
-    return;
 }
 
 void game()
